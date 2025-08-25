afafdfcea6ed9ba76771c1bcc549e16e28b77038
mohinigurjar <gurjarmohini112@gmail.com>
2025-08-25 23:23:54 +0530

    handling api

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,54 @@ const Card = require('./models/flashcard');
 
 app.use(express.json());
 
+//creating a new instance of the flashcard model
+app.post('/flashcard', async(req, res) => {
+    const flashcard = new Card(req.body);
+
+    try{
+        const newCard = await flashcard.save();
+        console.log(newCard);
+        res.send("New Flashcard added")
+    }catch(error){
+        res.status(500).send("Something went wrong");
+    }
+})
+
+//get all flashcards
+app.get('/flashcard', async(req, res) => {
+    try{
+        const allCards = await Card.find({});
+        res.send(allCards);
+    }catch{
+        res.status(404).send("Flashcards not found");
+    }
+})
+
+//delete card by id
+app.delete('/flashcard', async(req, res) => {
+    try{
+        const deleteCardId = req.body.deleteCardId;
+        const card = await Card.findByIdAndDelete(deleteCardId);
+        res.send("Card deleted successfully");
+
+    }catch(error){
+        res.status(404).send("card not found");
+
+    }
+})
+
+//updating particular field
+app.patch('/flashcard', async(req, res) => {
+    const cardId = req.body.cardId;
+    const data = req.body;
+    try{     
+        const card = await Card.findByIdAndUpdate({_id : cardId}, data);
+        res.send("card updated successfully");
+        // console.log(card);
+    }catch(err){
+        res.status(400).send("Updation failed" + err.message);
+    }
+})
 
 //creating a new instance of the user model
 app.post('/signup', async(req, res) => {
@@ -17,52 +65,6 @@ app.post('/signup', async(req, res) => {
     res.send("User added successfully")
     }catch(error){
         res.status(400).send("Error sending request" + error.message);
-    }
-
-})
-
-//fetching all users
-app.get('/users', async(req, res) => {
-    const users = await User.find({});
-    try{
-        res.send(users);
-    }catch(error){
-        res.status(404).send("Users not found");
-    }
-    
-})
-
-//delete user by id
-app.delete('/user', async(req, res) => {
-    const deleteUserId = req.body.deleteUserId;
-    try{
-        const user = await User.findByIdAndDelete(deleteUserId)
-        res.send("User deleted successfully");
-    }catch(error){
-        res.status(404).send("User not found");
-    }
-    
-})
-
-//creating a new instance of the flashcard model
-app.post('/flashcard', async(req, res) => {
-    const flashcard = new Card(req.body);
-
-    try{
-        const newCard = await flashcard.save();
-        res.send("New Flashcard added")
-    }catch(error){
-        res.status(500).send("Cannot create flashcard");
-    }
-})
-
-//get all flashcards
-app.get('/flashcard', async(req, res) => {
-    const allCards = await Card.find({});
-    try{
-        res.send(allCards);
-    }catch{
-        res.status(404).send("User not found");
     }
 })
 
--- backend/src/models/flashcard.js
+++ backend/src/models/flashcard.js
@@ -3,15 +3,18 @@ const mongoose = require("mongoose");
 const cardSchema = mongoose.Schema({
     question: {
         type: String,
+        required: true
     },
     answer: {
         type: String,
+        required: true
     },
     bookmarked: {
         type: Boolean,
     },
     tags: {
         type: [String],
+        required: true
     }
 })
 
